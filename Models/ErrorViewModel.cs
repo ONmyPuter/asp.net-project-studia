3c631799ee9b8fb045c9e38a7500ac02c60ac917
wiktoria <wiktoria.ference5@gmail.com>
2026-01-05 23:09:01 +0100

    Init project, setup gitignore

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarReservationSystemApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
